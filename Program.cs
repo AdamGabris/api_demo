8ae1a178e2173b9d3b7843cadc6ae65c1ff24307
AdamGabris <adamga@student.uia.no>
2024-03-12 10:02:59 +0100

    Pantry ID acquisition an storage.

--- Program.cs
+++ Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿const string CONFIG_FILE = ".config";
+string pantry_id = "";
+
+if (File.Exists(CONFIG_FILE))
+{
+    string[] configDetails = File.ReadAllLines(CONFIG_FILE);
+    pantry_id = configDetails[0];
+}
+else
+{
+    Console.WriteLine("Input your PantryID: ");
+    pantry_id = Console.ReadLine() ?? "";
+    if (pantry_id.Length > 0)
+    {
+        File.WriteAllText(CONFIG_FILE, pantry_id);
+    }
+}
